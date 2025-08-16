d3957c476604826e8e6fb58423a9d49c28175ecc
Anuska Chakraborty <anuskachakraborty2022@gmail.com>
2025-08-16 18:58:25 +0530

    Initial commit

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,92 @@
+import React, { useContext, useEffect, useState } from 'react'
+import withAuth from '../utils/withAuth'
+import { useNavigate } from 'react-router-dom'
+import "../App.css";
+import { Button, IconButton, TextField } from '@mui/material';
+import RestoreIcon from '@mui/icons-material/Restore';
+import { AuthContext } from '../contexts/AuthContext';
+import axios from 'axios';
+
+function HomeComponent() {
+
+
+    let navigate = useNavigate();
+    const [meetingCode, setMeetingCode] = useState("");
+    const [users, setUsers] = useState([]);
+
+
+    const {addToUserHistory} = useContext(AuthContext);
+    let handleJoinVideoCall = async () => {
+        await addToUserHistory(meetingCode)
+        navigate(`/${meetingCode}`)
+    }
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/v1/users")
+          .then(res => setUsers(res.data))
+          .catch(err => console.error("API error:", err));
+      }, []);
+
+    return (
+        <>
+
+            <div className="navBar">
+
+                <div style={{ display: "flex", alignItems: "center" }}>
+
+                    <h2>Apna Video Call</h2>
+                </div>
+
+                <div style={{ display: "flex", alignItems: "center" }}>
+                    <IconButton onClick={
+                        () => {
+                            navigate("/history")
+                        }
+                    }>
+                        <RestoreIcon />
+                    </IconButton>
+                    <p>History</p>
+
+                    <Button onClick={() => {
+                        localStorage.removeItem("token")
+                        navigate("/auth")
+                    }}>
+                        Logout
+                    </Button>
+                </div>
+
+
+            </div>
+
+
+            <div className="meetContainer">
+                <div className="leftPanel">
+                    <div>
+                        <h2>Providing Quality Video Calls</h2>
+
+                        <div style={{ display: 'flex', gap: "10px" }}>
+
+                            <TextField onChange={e => setMeetingCode(e.target.value)} id="outlined-basic" label="Meeting Code" variant="outlined" />
+                            <Button onClick={handleJoinVideoCall} variant='contained'>Join</Button>
+
+                        </div>
+                    </div>
+                </div>
+                <div className='rightPanel'>
+                    <img srcSet='/logo3.png' alt="" />
+                </div>
+            </div>
+            <div>
+                <h1>Users</h1>
+                <ul>
+                    {users.map(u => (
+                    <li key={u._id}>{u.name}</li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
+}
+
+
+export default withAuth(HomeComponent)
